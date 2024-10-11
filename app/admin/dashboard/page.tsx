c844017884c4e1d6caa8a025366fd703f17d4bcb
Mongoljan Sabyrjan <mongoljansabyrjan@Mongoljans-MacBook-Pro.local>
2024-10-11 16:53:36 +0800

    changes

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -10,6 +10,10 @@ export default function AdminDashboard() {
     <div className=" text-black">
       <h1>Admin Dashboard</h1>
       <p>Welcome, </p> {/* Display user info */}
+      <div>
+        added some changes
+        
+      </div>
     </div>
   );
 }
