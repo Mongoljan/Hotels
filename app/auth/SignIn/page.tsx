7b74dd555ba1b373c4decb3296020af8cc003ab7
Mongoljan Sabyrjan <mongoljansabyrjan@Mongoljans-MacBook-Pro.local>
2024-10-11 23:21:54 +0800

    changes

--- app/auth/SignIn/page.tsx
+++ app/auth/SignIn/page.tsx
@@ -26,48 +26,48 @@ export default function LoginPage() {
     resolver: zodResolver(schemaLogin),
   });
 
-  const onSubmit: SubmitHandler<FormFields> = async (data) => {
-    try {
-      const requestBody = {
-        email: data.email,
-        password: data.password,
-      };
+  // const onSubmit: SubmitHandler<FormFields> = async (data) => {
+  //   try {
+  //     const requestBody = {
+  //       email: data.email,
+  //       password: data.password,
+  //     };
 
-      const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/hotel-owner/login/`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(requestBody),
-      });
+  //     const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/hotel-owner/login/`, {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify(requestBody),
+  //     });
 
-      if (response.ok) {
-        const responseData = await response.json();
+  //     if (response.ok) {
+  //       const responseData = await response.json();
 
-        // Store the token in a cookie (not HTTP-only, accessible by client-side JavaScript)
-        Cookies.set('jwtToken', responseData.token, {
-          expires: 0.02083,  // 1 day expiration
-          secure: process.env.NODE_ENV === 'production',  // Use secure cookies in production
-          sameSite: 'Strict',  // Ensure cookie is sent only with requests from the same site
-        });
+  //       // Store the token in a cookie (not HTTP-only, accessible by client-side JavaScript)
+  //       Cookies.set('jwtToken', responseData.token, {
+  //         expires: 0.02083,  // 1 day expiration
+  //         secure: process.env.NODE_ENV === 'production',  // Use secure cookies in production
+  //         sameSite: 'Strict',  // Ensure cookie is sent only with requests from the same site
+  //       });
 
-        toast.success('Login successful!');
-        router.push('/admin/dashboard'); // Redirect after successful login
-      } else {
-        const errorData = await response.json();
-        toast.error(errorData.message || 'Login failed. Please check your input.');
-      }
-    } catch (error) {
-      toast.error('An unexpected error occurred during login');
-      console.error('Error during login:', error);
-    }
-  };
+  //       toast.success('Login successful!');
+  //       router.push('/admin/dashboard'); // Redirect after successful login
+  //     } else {
+  //       const errorData = await response.json();
+  //       toast.error(errorData.message || 'Login failed. Please check your input.');
+  //     }
+  //   } catch (error) {
+  //     toast.error('An unexpected error occurred during login');
+  //     console.error('Error during login:', error);
+  //   }
+  // };
 
   return (
     <div className="flex bg-[#E5FDoD] justify-center items-center min-h-screen h-full py-[100px]  rounded-[20px]">
       <ToastContainer />
       <form
-        onSubmit={handleSubmit(onSubmit)}
+        // onSubmit={handleSubmit(onSubmit)}
         className="bg-white p-10 px-20 max-w-[600px] md:min-w-[550px] min-w-[250px] rounded-md  text-gray-600"
       >
         <h2 className="text-2xl font-bold mx-auto text-center text-blue-500 mb-10">Нэвтрэх</h2>
