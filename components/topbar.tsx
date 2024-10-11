7cf5257936ac75b24c5697e069a664b613c3de2a
Mongoljan Sabyrjan <mongoljansabyrjan@Mongoljans-MacBook-Pro.local>
2024-10-11 23:29:32 +0800

    changes

--- components/topbar.tsx
+++ components/topbar.tsx
@@ -9,14 +9,14 @@ export default function Topbar() {
         <div className="flex gap-4 items-center justify-end h-full ">
           <Link
             className="rounded-sm border border-solid text-xs  border-blue-500 transition-colors flex items-center justify-center hover:bg-blue-500  hover:border-transparent  sm:text-base h-8 sm:h-10 px-2 sm:px-2 sm:min-w-36 text-blue-500 hover:text-white"
-            href={"/auth/signIn"}
+            href={"/auth/SignIn"}
           >
             Нэвтрэх
           </Link>
 
           <Link
             className="rounded-sm border border-solid  border-blue-500 transition-colors flex items-center justify-center hover:bg-blue-500  hover:border-transparent text-xs sm:text-base h-8 sm:h-10 px-2 sm:px-2 sm:min-w-36 text-blue-500 hover:text-white"
-            href={"/auth/signUp"}
+            href={"/auth/SignUp"}
           >
             Бүртгүүлэх
           </Link>
